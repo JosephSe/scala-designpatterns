34e069dddf263340f390d2905b4637120bd7b006
Joseph Sebastian <subins@gmail.com>
2015-08-28 18:19:05 +0100

    Adapter pattern

--- src/scala/designpatterns/AdapterPatternApplication.scala
+++ src/scala/designpatterns/AdapterPatternApplication.scala
@@ -12,3 +12,19 @@ object WARNING extends Level
 
 object ERROR extends Level
 
+class Logger {
+  def log(level: Level, mess: String): Unit = print(s"$level-$mess")
+}
+
+class LoggerToAdapter(logger: Logger) extends Log {
+
+  override def warn(mess: String): Unit = logger.log(WARNING, mess)
+
+  override def err(mess: String): Unit = logger.log(ERROR, mess)
+}
+
+object AdapterPatternApplication extends App {
+
+  new LoggerToAdapter(new Logger).warn("here is your warning")
+
+}
