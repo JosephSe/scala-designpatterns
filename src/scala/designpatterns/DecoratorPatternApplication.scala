d703c1fcb362e25a7c65c7e6f2a6efbb75c5c13b
Joseph Sebastian <subins@gmail.com>
2015-09-16 00:05:34 +0100

    DecoratorPatternApplication

--- src/scala/designpatterns/DecoratorPatternApplication.scala
+++ src/scala/designpatterns/DecoratorPatternApplication.scala
@@ -9,3 +9,14 @@ trait RectangleShap extends Shape {
   override def draw: Unit = println("Drawing Rectangle")
 }
 
+trait SquareShape extends Shape {
+  override def draw: Unit = println("Drawing Square")
+}
+
+class Image(width: Int, hight: Int)
+
+object DecoratorPatternApplication extends App {
+
+  new Image(10, 20) with RectangleShap draw
+
+}
