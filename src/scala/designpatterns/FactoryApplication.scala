e583a023d347b6ba7796c6298efdd444466ef1d3
Joseph Sebastian <subins@gmail.com>
2015-10-03 23:49:05 +0100

    FactoryPatternApplication

--- src/scala/designpatterns/FactoryApplication.scala
+++ src/scala/designpatterns/FactoryApplication.scala
@@ -12,3 +12,13 @@ class Cat extends Animal {
   override def makeSound: Unit = println("Meow Meow")
 }
 
+object Animal {
+  def apply(typ: String): Animal = typ match {
+    case "dog" => new Dog()
+    case "cat" => new Cat()
+  }
+}
+
+object FactoryApplication extends App {
+  Animal("dog").makeSound
+}
